8dbb26311a4f190c2f2ebc3313eb87b22f9bcf5a
Emanuele1927 <35915808+Emanuele1927@users.noreply.github.com>
2021-02-15 16:52:27 +0100

    Completata parte client

--- catalogoProdotto-ill-client/src/app/app.component.ts
+++ catalogoProdotto-ill-client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Prodotto } from './prodotto';
+import { RichiestaDto } from './richiesta-dto';
 import { RispostaDto } from './risposta-dto';
 
 @Component({
@@ -21,6 +22,37 @@ export class AppComponent {
       .get<RispostaDto>(this.url + "recupera");
     oss.subscribe(p => {
       this.prodotti = p.listaProdotti;
+    });
+  }
+
+  aggiungi() {
+    let req = new RichiestaDto();
+    req.prodotto = this.prodotto;
+
+    let oss: Observable<RispostaDto> = this.http
+      .post<RispostaDto>(this.url + "aggiungi", req);
+    oss.subscribe(p => {
+      this.prodotti = p.listaProdotti;
+    });
+    this.prodotto = new Prodotto();
+  }
+
+  svuota() {
+    let oss: Observable<RispostaDto> = this.http
+      .get<RispostaDto>(this.url + "svuota");
+    oss.subscribe(r => {
+      this.prodotti = r.listaProdotti;
+    });
+  }
+
+  elimina(p: Prodotto) {
+    let req = new RichiestaDto();
+    req.prodotto = p;
+
+    let oss: Observable<RispostaDto> = this.http
+      .post<RispostaDto>(this.url + "elimina", req);
+    oss.subscribe(r => {
+      this.prodotti = r.listaProdotti;
     });
   }
 }
