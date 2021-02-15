4b3b6f06370d16c8e1d0a333a07c58beef97ad95
Giulia2Gargano <giulia2gargano@gmail.com>
2021-02-15 16:31:48 +0100

    aggiunte classi e metodo recupera

--- catalogoProdotto-ill-client/src/app/app.component.ts
+++ catalogoProdotto-ill-client/src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Prodotto } from './prodotto';
+import { RispostaDto } from './risposta-dto';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'catalogoProdotto-ill-client';
+  prodotto = new Prodotto();
+  prodotti: Prodotto[] = [];
+  url = "http://localhost:8080/";
+
+  constructor(private http: HttpClient) { }
+
+  recupera() {
+    let oss: Observable<RispostaDto> = this.http
+      .get<RispostaDto>(this.url + "recupera");
+    oss.subscribe(p => {
+      this.prodotti = p.listaProdotti;
+    });
+  }
 }
---
+++ catalogoProdotto-ill-client/src/app/richiesta-dto.ts
@@ -0,0 +1,5 @@
+import { Prodotto } from "./prodotto";
+
+export class RichiestaDto {
+    prodotto: Prodotto;
+}
