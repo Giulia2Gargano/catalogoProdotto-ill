d824a4f4a17cc29048b377750e7562c7483fa613
Giulia2Gargano <giulia2gargano@gmail.com>
2021-02-15 13:52:10 +0100

    primo commit

---
+++ catalogoProdotto-ill-server/src/main/java/it/sirfin/catalogoprodottoill/CatalogoprodottoillApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.catalogoprodottoill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoprodottoillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoprodottoillApplication.class, args);
+	}
+
+}
---
+++ catalogoProdotto-ill-server/src/test/java/it/sirfin/catalogoprodottoill/CatalogoprodottoillApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.catalogoprodottoill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoprodottoillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
